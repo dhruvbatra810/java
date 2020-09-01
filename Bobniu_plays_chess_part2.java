dc4e088832495560d73f8cc79365f46182fada07
dhruvbatra810 <60519182+dhruvbatra810@users.noreply.github.com>
2020-09-01 07:46:31 +0530

    Update Bobniu_plays_chess_part2.java

--- Bobniu_plays_chess_part2.java
+++ Bobniu_plays_chess_part2.java
@@ -1,3 +1,5 @@
+// code forces https://codeforces.com/problemset/problem/1395/B
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
